36afcec7a3c6bee69439b9cec8b845d24261ee63
CSNWEB <christoph@kiron.university>
2019-08-30 16:37:45 +0200

    Implement first version of questy builder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import yaml from "js-yaml";
+import "./App.css";
+import Form from "react-jsonschema-form";
+import schema from "./questy-schema.json";
 
 function App() {
+  const storedData = localStorage.getItem("formData");
+  const data = storedData ? JSON.parse(storedData) : {};
+  const [formData, setFormData] = useState(data);
+  console.log(formData);
+  const onChange = ({ formData }, e) => {
+    setFormData(formData);
+    localStorage.setItem("formData", JSON.stringify(formData));
+  };
+  const clear = () => {
+    localStorage.removeItem("formData");
+    setFormData({});
+  };
+
+  const prettifySchema = data => {
+    const pages = data.pages;
+    if (!pages || !Array.isArray(pages)) {
+      return data;
+    }
+    const newPages = pages.reduce((obj, page) => {
+      if (!page.questions || !Array.isArray(page.questions)) {
+        obj[page.denomination] = {};
+        return obj;
+      }
+      const newQuestions = page.questions.reduce((result, question) => {
+        const { identifier, ...newQuestion } = question;
+        result[identifier] = question;
+        return result;
+      }, {});
+      obj[page.denomination] = newQuestions;
+      return obj;
+    }, {});
+    return Object.assign({}, data, { pages: newPages });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <Form
+        schema={schema}
+        formData={formData}
+        onChange={onChange}
+        liveValidate={true}
+      >
+        <pre>
+          <code className="language-yaml">
+            {yaml.safeDump(prettifySchema(formData), { skipInvalid: true })}
+          </code>
+        </pre>
+      </Form>
+      <button className="btn btn-default" onClick={clear}>
+        Clear Data
+      </button>
     </div>
   );
 }
